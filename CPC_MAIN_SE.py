b5eb68113df454f2fd7e30d58f815454eb3cd0c2
Sirius1897 <Sirius97black@gmail.com>
2018-12-20 18:12:38 +0100

    TEST5

--- CPC_MAIN_SE.py
+++ CPC_MAIN_SE.py
@@ -14,7 +14,7 @@ Main_Loop = True
 while Main_Loop:
 
     left_x_signal = RC(RR(0), -1, 1)
-    left_y_signal = RC(RR(1, -1, 1))
+    left_y_signal = RC(RR(1), -1, 1)
     right_x_signal = RC(RR(2), -1, 1)
     right_y_signal = RC(RR(3), -1, 1)
 
