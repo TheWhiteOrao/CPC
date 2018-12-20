c8c84b19c8e69d1d10878ebae72aa9656312d3b3
Sirius1897 <Sirius97black@gmail.com>
2018-12-20 01:01:00 +0100

    Add imports to Main-, Server and Client

--- CPC_RR.py
+++ CPC_RR.py
@@ -5,9 +5,6 @@
 # ██   ██ ██      ██   ██ ██   ██     ██   ██ ██      ██      ██      ██  ██  ██  ██      ██   ██
 # ██   ██ ███████ ██   ██ ██████      ██   ██ ███████  ██████ ███████ ██   ████   ███████ ██   ██
 
-from sys import *
-from time import *
-
 from CPC_NAVIO2.rcinput import *
 from CPC_NAVIO2.util import *
 
@@ -16,5 +13,5 @@ check_apm()
 receiver_imput = RCInput()
 
 
-def receiver_channel(channel_number):
+def RR(channel_number):
     return receiver_imput.read(channel_number)
