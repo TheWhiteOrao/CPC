4348264254ae67ee50922a9d7b6bc30763ac5b11
Sirius1897 <Sirius97black@gmail.com>
2018-12-22 18:17:26 +0100

    sdf

--- CPC_MAIN_CL.py
+++ CPC_MAIN_CL.py
@@ -14,12 +14,4 @@ main_cl.connect(main_cl_addr)
 
 while True:
     k = main_cl.recv(1024).decode()
-    left_x_signal = k[0]
-    left_y_signal = k[1]
-    right_x_signal = k[2]
-    right_y_signal = k[3]
-
-    print("left_x: %f  " % left_x_signal,
-          "left_y: %f  " % left_y_signal,
-          "right_x: %f  " % right_x_signal,
-          "right_y: %f  " % right_y_signal)
+    print(k)
