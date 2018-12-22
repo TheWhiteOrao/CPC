810be8d310b61461d01e7f988380acc02c9098e8
Sirius1897 <Sirius97black@gmail.com>
2018-12-22 17:18:05 +0100

    test1

--- CPC_MAIN_SE.py
+++ CPC_MAIN_SE.py
@@ -12,6 +12,13 @@ from CPC_NAVIO2.leds import *
 
 main_loop = True
 led_time = 0
+sim = True
+
+if sim == True:
+    main_se = socket(AF_INET, SOCK_STREAM)
+    main_se.bind("192.168.43.243", 35467)
+    main_se.listen(0)
+    (main_cl, addr) = main_se.accept()
 
 
 while main_loop:
@@ -28,6 +35,12 @@ while main_loop:
     left_x_signal = RC(RR(3), -1, 1)
     right_x_signal = RC(RR(1), -1, 1)
     right_y_signal = RC(RR(2), -1, 1)
+
+    if sim == True:
+        main_cl.send(eft_y_signal.encode())
+        main_cl.send(eft_x_signal.encode())
+        main_cl.send(right_x_signal.encode())
+        main_cl.send(right_y_signal.encode())
 
     print("left_x: %f  " % left_x_signal,
           "left_y: %f  " % left_y_signal,
