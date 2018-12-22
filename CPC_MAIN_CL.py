a65a4e8afcc227e019512703c265feeb7115057e
Sirius1897 <Sirius97black@gmail.com>
2018-12-22 20:12:25 +0100

    efdr

--- CPC_MAIN_CL.py
+++ CPC_MAIN_CL.py
@@ -14,9 +14,14 @@ main_cl_addr = ("192.168.43.34", 35467)
 main_cl.connect(main_cl_addr)
 
 while True:
-    all = (main_cl.recv(4096))
-
+    all = main_cl.recv(4096)
     try:
-        loads(all)
+        all = loads(all)
+
     except:
-        print("H")
+        pass
+
+    print("left_x: %f  " % all[0],
+          "left_y: %f  " % all[1],
+          "right_x: %f  " % all[2],
+          "right_y: %f  " % all[3])
