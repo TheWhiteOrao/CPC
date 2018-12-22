e58636411c172153e3c18de161e446210b5056c8
Sirius1897 <Sirius97black@gmail.com>
2018-12-22 19:23:49 +0100

    as

--- CPC_MAIN_CL.py
+++ CPC_MAIN_CL.py
@@ -14,7 +14,8 @@ main_cl_addr = ("192.168.43.34", 35467)
 main_cl.connect(main_cl_addr)
 
 while True:
-    all = main_cl.recv(1024)
+    all = main_cl.recv(4096)
+    print(all)
     all = loads(all)
 
     print("left_x: %f  " % all[0],
