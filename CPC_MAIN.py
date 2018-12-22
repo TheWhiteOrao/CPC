6ce671e9c1c8d80fa620a0e64c0433eb1f9a9934
Sirius1897 <Sirius97black@gmail.com>
2018-12-22 23:45:27 +0100

    pfdg

--- CPC_MAIN.py
+++ CPC_MAIN.py
@@ -14,29 +14,29 @@ main_loop = True
 led_time = 0
 
 pwm_out = 1
-with PWM(pwm_out) as pwm:
+pwm = PWM(pwm_out)
+pwm.initialize()
+pwm.set_period(500)
+pwm.enable()
 
-    pwm.set_period(500)
-    pwm.enable()
-
-    while main_loop:
-
-        if led_time == 1000:
-            Led().setColor('Green')
-        if led_time == 2000:
-            Led().setColor('Black')
-            led_time = 0
-
-        led_time += 1
-
-        left_y_signal = RC(RR(0), 1.100, 1.940)
-        left_x_signal = RC(RR(3), -1, 1)
-        right_x_signal = RC(RR(1), -1, 1)
-        right_y_signal = RC(RR(2), -1, 1)
-
-        pwm.set_duty_cycle(left_y_signal)
-
-        print("left_x: %f  " % left_x_signal,
-              "left_y: %f  " % left_y_signal,
-              "right_x: %f  " % right_x_signal,
-              "right_y: %f  " % right_y_signal)
+# while main_loop:
+#
+#     if led_time == 1000:
+#         Led().setColor('Green')
+#     if led_time == 2000:
+#         Led().setColor('Black')
+#         led_time = 0
+#
+#     led_time += 1
+#
+#     left_y_signal = RC(RR(0), 1.100, 1.940)
+#     left_x_signal = RC(RR(3), -1, 1)
+#     right_x_signal = RC(RR(1), -1, 1)
+#     right_y_signal = RC(RR(2), -1, 1)
+#
+#     pwm.set_duty_cycle(left_y_signal)
+#
+#     print("left_x: %f  " % left_x_signal,
+#           "left_y: %f  " % left_y_signal,
+#           "right_x: %f  " % right_x_signal,
+#           "right_y: %f  " % right_y_signal)
