3645aaffe853ccce8ed85214006baac69039fd7d
Sirius1897 <Sirius97black@gmail.com>
2018-12-22 18:03:09 +0100

    sdyf

--- CPC_MAIN_CL.py
+++ CPC_MAIN_CL.py
@@ -13,4 +13,13 @@ main_cl_addr = ("192.168.43.34", 35467)
 main_cl.connect(main_cl_addr)
 
 while True:
-    print(main_cl.recv(1024).decode())
+    k = main_cl.recv(1024).decode()
+    left_x_signal = k[0]
+    left_y_signal = k[1]
+    right_x_signal = k[2]
+    right_y_signal = k[3]
+
+    print("left_x: %f  " % left_x_signal,
+          "left_y: %f  " % left_y_signal,
+          "right_x: %f  " % right_x_signal,
+          "right_y: %f  " % right_y_signal)
