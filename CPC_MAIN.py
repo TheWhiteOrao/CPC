95504a3efd8ccbf2a02c944b51fe2668877a6f46
Sirius1897 <Sirius97black@gmail.com>
2018-12-22 20:55:20 +0100

    xc

--- CPC_MAIN.py
+++ CPC_MAIN.py
@@ -13,31 +13,30 @@ from CPC_NAVIO2.pwm import *
 main_loop = True
 led_time = 0
 
-pwm_out = 0
-f = PWM(pwm_out).initialize()
-with PWM(pwm_out) as pwm_t:
-    print(pwm_t == f)
-    pwm_t.set_period(500)
-    pwm_t.enable()
+pwm_out = 1
+with PWM(pwm_out) as pwm:
 
-    # while main_loop:
-    #
-    #     if led_time == 1000:
-    #         Led().setColor('Green')
-    #     if led_time == 2000:
-    #         Led().setColor('Black')
-    #         led_time = 0
-    #
-    #     led_time += 1
-    #
-    #     left_y_signal = RC(RR(0), 1.100, 1.940)
-    #     left_x_signal = RC(RR(3), -1, 1)
-    #     right_x_signal = RC(RR(1), -1, 1)
-    #     right_y_signal = RC(RR(2), -1, 1)
-    #
-    #     pwm_t.set_duty_cycle(left_y_signal)
-    #
-    #     print("left_x: %f  " % left_x_signal,
-    #           "left_y: %f  " % left_y_signal,
-    #           "right_x: %f  " % right_x_signal,
-    #           "right_y: %f  " % right_y_signal)
+    pwm.set_period(500)
+    pwm.enable()
+
+    while main_loop:
+
+        if led_time == 1000:
+            Led().setColor('Green')
+        if led_time == 2000:
+            Led().setColor('Black')
+            led_time = 0
+
+        led_time += 1
+
+        left_y_signal = RC(RR(0), 1.100, 1.940)
+        left_x_signal = RC(RR(3), -1, 1)
+        right_x_signal = RC(RR(1), -1, 1)
+        right_y_signal = RC(RR(2), -1, 1)
+
+        pwm.set_duty_cycle(left_y_signal)
+
+        print("left_x: %f  " % left_x_signal,
+              "left_y: %f  " % left_y_signal,
+              "right_x: %f  " % right_x_signal,
+              "right_y: %f  " % right_y_signal)
