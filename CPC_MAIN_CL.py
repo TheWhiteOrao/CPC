9df2122700166749b22f3214f1255f6b9b6fef2f
Sirius1897 <Sirius97black@gmail.com>
2018-12-22 20:08:59 +0100

    RDFG

--- CPC_MAIN_CL.py
+++ CPC_MAIN_CL.py
@@ -14,6 +14,9 @@ main_cl_addr = ("192.168.43.34", 35467)
 main_cl.connect(main_cl_addr)
 
 while True:
-    all = (main_cl.recv(512))
+    all = (main_cl.recv(4096))
 
-    print(loads(all))
+    try:
+        loads(all)
+    except:
+        print("H")
--- CPC_MAIN_SE.py
+++ CPC_MAIN_SE.py
@@ -39,10 +39,7 @@ while main_loop:
 
     if sim == True:
 
-        main_cl.sendall(dumps((left_y_signal,
-                               left_x_signal,
-                               right_x_signal,
-                               right_y_signal)))
+        main_cl.send(dumps((left_y_signal, left_x_signal, right_x_signal, right_y_signal)))
 
     print("left_x: %f  " % left_x_signal,
           "left_y: %f  " % left_y_signal,
