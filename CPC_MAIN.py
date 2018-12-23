66dbbbafb1a892375be4ccdd1da728ab27a4191b
Sirius1897 <Sirius97black@gmail.com>
2018-12-23 23:24:38 +0100

    fsd

--- CPC_MAIN.py
+++ CPC_MAIN.py
@@ -10,15 +10,45 @@
 
 from CPC_RR import *
 from CPC_RC import *
+from CPC_CI import *
 from CPC_NAVIO2.leds import *
 from CPC_NAVIO2.pwm import *
 
 main_loop = True
 led_time = 0
+engine_min = 1.100
+engine_max = 1.940
+engine_cp = 0.25
+engine_range = engine_max - engine_min
+engine_cs = engine_range * (1 - engine_cp) + engine_min
+engine_cp_range = engine_range * engine_cp
 
 
-# pwm.set_period(500)
-# pwm.enable()
+# ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████
+#
+#  ██████  ██████  ██████  ██████  ████████ ███████ ██████      ██ ███    ██ ██ ████████ ██  █████  ██      ██ ███████ ███████
+# ██      ██    ██ ██   ██ ██   ██    ██    ██      ██   ██     ██ ████   ██ ██    ██    ██ ██   ██ ██      ██    ███  ██
+# ██      ██    ██ ██████  ██████     ██    █████   ██████      ██ ██ ██  ██ ██    ██    ██ ███████ ██      ██   ███   █████
+# ██      ██    ██ ██      ██         ██    ██      ██   ██     ██ ██  ██ ██ ██    ██    ██ ██   ██ ██      ██  ███    ██
+#  ██████  ██████  ██      ██         ██    ███████ ██   ██     ██ ██   ████ ██    ██    ██ ██   ██ ███████ ██ ███████ ███████
+#
+# ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████
+
+engine_one_x = 90
+engine_one_y = 0
+
+engine_two_x = 18
+engine_two_y = 72
+
+engine_thr_x = 54
+engine_thr_y = 36
+
+engine_fou_x = 54
+engine_fou_y = 36
+
+engine_fiv_x = 18
+engine_fiv_y = 72
+
 
 # ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████
 #
@@ -30,33 +60,33 @@ led_time = 0
 #
 # ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████
 
-engine_one = PWM(0)
-engine_two = PWM(1)
-engine_thr = PWM(2)
-engine_fou = PWM(3)
-engine_fiv = PWM(4)
-engine_six = PWM(5)
+engine_one_pwm = PWM(0)
+engine_two_pwm = PWM(1)
+engine_thr_pwm = PWM(2)
+engine_fou_pwm = PWM(3)
+engine_fiv_pwm = PWM(4)
+engine_six_pwm = PWM(5)
 
-engine_one.initialize()
-engine_two.initialize()
-engine_thr.initialize()
-engine_fou.initialize()
-engine_fiv.initialize()
-engine_six.initialize()
+engine_one_pwm.initialize()
+engine_two_pwm.initialize()
+engine_thr_pwm.initialize()
+engine_fou_pwm.initialize()
+engine_fiv_pwm.initialize()
+engine_six_pwm.initialize()
 
-engine_one.set_period(500)
-engine_two.set_period(500)
-engine_thr.set_period(500)
-engine_fou.set_period(500)
-engine_fiv.set_period(500)
-engine_six.set_period(500)
+engine_one_pwm.set_period(500)
+engine_two_pwm.set_period(500)
+engine_thr_pwm.set_period(500)
+engine_fou_pwm.set_period(500)
+engine_fiv_pwm.set_period(500)
+engine_six_pwm.set_period(500)
 
-engine_one.enable()
-engine_two.enable()
-engine_thr.enable()
-engine_fou.enable()
-engine_fiv.enable()
-engine_six.enable()
+engine_one_pwm.enable()
+engine_two_pwm.enable()
+engine_thr_pwm.enable()
+engine_fou_pwm.enable()
+engine_fiv_pwm.enable()
+engine_six_pwm.enable()
 
 # ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████
 #
@@ -78,17 +108,17 @@ while main_loop:
 
     led_time += 1
 
-    left_y_signal = RC(RR(0), 1.100, 1.940)
-    left_x_signal = RC(RR(3), -1, 1)
-    right_x_signal = RC(RR(1), -1, 1)
-    right_y_signal = RC(RR(2), -1, 1)
+    left_y_signal = RC(RR(0), engine_min, engine_cs)
+    left_x_signal = RC(RR(3), -engine_cp_range, engine_cp_range)
+    right_x_signal = RC(RR(1), -engine_cp_range, engine_cp_range)
+    right_y_signal = RC(RR(2), -engine_cp_range, engine_cp_range)
 
-    engine_one.set_duty_cycle(left_y_signal)
-    engine_two.set_duty_cycle(left_y_signal)
-    engine_thr.set_duty_cycle(left_y_signal)
-    engine_fou.set_duty_cycle(left_y_signal)
-    engine_fiv.set_duty_cycle(left_y_signal)
-    engine_six.set_duty_cycle(left_y_signal)
+    engine_one_pwm.set_duty_cycle(left_y_signal)
+    engine_two_pwm.set_duty_cycle(left_y_signal)
+    engine_thr_pwm.set_duty_cycle(left_y_signal)
+    engine_fou_pwm.set_duty_cycle(left_y_signal)
+    engine_fiv_pwm.set_duty_cycle(left_y_signal)
+    engine_six_pwm.set_duty_cycle(left_y_signal)
 
     print("left_x: %f  " % left_x_signal,
           "left_y: %f  " % left_y_signal,
--- CPC_TF.py
+++ CPC_TF.py
@@ -1,5 +1,7 @@
-from pickle import *
-
-h = dumps((1, 2, 3))
-k = h
-print(loads(\x00))
+engine_min = 1.100
+engine_max = 1.940
+engine_cp = 0.25
+engine_range = engine_max - engine_min
+engine_cs = engine_range * (1 - engine_cp) + engine_min
+engine_cp_range = engine_range * engine_cp
+print(engine_cs, engine_cp_range)
