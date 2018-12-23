e074217a28cc89bd2700b140320ed4e305a8f558
Sirius1897 <Sirius97black@gmail.com>
2018-12-24 00:56:31 +0100

    sd

--- CPC_MAIN.py
+++ CPC_MAIN.py
@@ -10,6 +10,7 @@
 
 from CPC_RR import *
 from CPC_RC import *
+from CPC_CO import *
 from CPC_NAVIO2.leds import *
 from CPC_NAVIO2.pwm import *
 
@@ -18,9 +19,9 @@ led_time = 0
 engine_min = 1.100
 engine_max = 1.940
 engine_cp = 0.25
+engine_e = 0.45
 engine_range = engine_max - engine_min
 engine_cs = engine_range * (1 - engine_cp) + engine_min
-engine_cp_range = engine_range * engine_cp
 
 
 # ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████
@@ -32,8 +33,12 @@ engine_cp_range = engine_range * engine_cp
 #  ██████  ██████  ██      ██         ██    ███████ ██   ██     ██ ██   ████ ██    ██    ██ ██   ██ ███████ ██ ███████ ███████
 #
 # ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████
+rotor_radius = 500
+engine_force = 10000
 
-engine_one_x = 90
+basic_lever = engine_force * rotor_radius
+
+# engine_one_x = 90
 engine_one_y = 0
 
 engine_two_x = 18
@@ -47,6 +52,72 @@ engine_fou_y = 36
 
 engine_fiv_x = 18
 engine_fiv_y = 72
+
+# engine_one_length_to_x = CO(engine_one_x) * rotor_radius
+engine_one_length_to_y = CO(engine_one_y) * rotor_radius
+
+engine_two_length_to_x = CO(engine_two_x) * rotor_radius
+engine_two_length_to_y = CO(engine_two_y) * rotor_radius
+
+engine_thr_length_to_x = CO(engine_thr_x) * rotor_radius
+engine_thr_length_to_y = CO(engine_thr_y) * rotor_radius
+
+engine_fou_length_to_x = CO(engine_fou_x) * rotor_radius
+engine_fou_length_to_y = CO(engine_fou_y) * rotor_radius
+
+engine_fiv_length_to_x = CO(engine_fiv_x) * rotor_radius
+engine_fiv_length_to_y = CO(engine_fiv_y) * rotor_radius
+
+# engine_one_lever_x = basic_lever / (engine_one_length_to_x * engine_force)
+engine_one_lever_y = basic_lever / (engine_one_length_to_y * engine_force)
+
+engine_two_lever_x = basic_lever / (engine_two_length_to_x * engine_force)
+engine_two_lever_y = basic_lever / (engine_two_length_to_y * engine_force)
+
+engine_thr_lever_x = basic_lever / (engine_thr_length_to_x * engine_force)
+engine_thr_lever_y = basic_lever / (engine_thr_length_to_y * engine_force)
+
+engine_fou_lever_x = basic_lever / (engine_fou_length_to_x * engine_force)
+engine_fou_lever_y = basic_lever / (engine_fou_length_to_y * engine_force)
+
+engine_fiv_lever_x = basic_lever / (engine_fiv_length_to_x * engine_force)
+engine_fiv_lever_y = basic_lever / (engine_fiv_length_to_y * engine_force)
+
+
+# f = engine_125
+
+engine_one_f = engine_one_lever_y / 3
+engine_two_f = engine_two_lever_y / 3
+engine_fiv_f = engine_fiv_lever_y / 3
+
+# r = engine_23
+
+engine_two_r = engine_two_lever_x / 2
+engine_thr_r = engine_thr_lever_x / 2
+
+# b = engine_34
+
+engine_thr_b = engine_thr_lever_y / 2
+engine_fou_b = engine_fou_lever_y / 2
+
+# l = engine 45
+
+engine_fou_l = engine_fou_lever_x / 2
+engine_fiv_l = engine_fiv_lever_x / 2
+
+
+print(0.25 * (engine_one_f * 0.9), "f")
+print(0.25 * (engine_two_f * 0.9), "f")
+print(0.25 * (engine_fiv_f * 0.9), "f \n")
+
+print(0.25 * (engine_two_r * 0.9), "r")
+print(0.25 * (engine_thr_r * 0.9), "r \n")
+
+print(0.25 * (engine_thr_b * 0.9), "b")
+print(0.25 * (engine_fou_b * 0.9), "b \n")
+
+print(0.25 * (engine_fou_l * 0.9), "l")
+print(0.25 * (engine_fiv_l * 0.9), "l \n")
 
 
 # ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████ ███████
@@ -108,16 +179,60 @@ while main_loop:
     led_time += 1
 
     left_y_signal = RC(RR(0), engine_min, engine_cs)
-    left_x_signal = RC(RR(3), -engine_cp_range, engine_cp_range)
-    right_x_signal = RC(RR(1), -engine_cp_range, engine_cp_range)
-    right_y_signal = RC(RR(2), -engine_cp_range, engine_cp_range)
+    left_x_signal = RC(RR(3), -engine_cp, engine_cp)
 
-    engine_one_pwm.set_duty_cycle(left_y_signal)
-    engine_two_pwm.set_duty_cycle(left_y_signal)
-    engine_thr_pwm.set_duty_cycle(left_y_signal)
-    engine_fou_pwm.set_duty_cycle(left_y_signal)
-    engine_fiv_pwm.set_duty_cycle(left_y_signal)
-    engine_six_pwm.set_duty_cycle(left_y_signal)
+    right_x_signal = RC(RR(1), -engine_cp, engine_cp)
+    right_y_signal = RC(RR(2), -engine_cp, engine_cp)
+
+    engine_one_memory = left_y_signal
+    engine_two_memory = left_y_signal
+    engine_thr_memory = left_y_signal
+    engine_fou_memory = left_y_signal
+    engine_fiv_memory = left_y_signal
+    engine_six_memory = left_y_signal
+
+    if left_x_signal < 0:
+        engine_six_memory += engine_range * (abs(left_x_signal))
+    elif left_x_signal > 0:
+        engine_six_memory -= engine_range * (abs(left_x_signal))
+
+    if right_x_signal < 0:  # - to_the_left engine_r inC engine_l deC
+        engine_two_memory += engine_range * (abs(right_x_signal) * (engine_two_r * engine_e))
+        engine_thr_memory += engine_range * (abs(right_x_signal) * (engine_thr_r * engine_e))
+        engine_fou_memory -= engine_range * (abs(right_x_signal) * (engine_fou_l * engine_e))
+        engine_fiv_memory -= engine_range * (abs(right_x_signal) * (engine_fiv_l * engine_e))
+    elif right_x_signal > 0:  # + to_the_right engine_r deC engine_l inC
+        engine_two_memory -= engine_range * (abs(right_x_signal) * (engine_two_r * engine_e))
+        engine_thr_memory -= engine_range * (abs(right_x_signal) * (engine_thr_r * engine_e))
+        engine_fou_memory += engine_range * (abs(right_x_signal) * (engine_fou_l * engine_e))
+        engine_fiv_memory += engine_range * (abs(right_x_signal) * (engine_fiv_l * engine_e))
+    else:
+        pass
+
+    if right_y_signal < 0:  # - to_the_back engine_b deC engine_f inC
+        engine_one_memory += engine_range * (abs(right_y_signal) * (engine_one_f * engine_e))
+        engine_two_memory += engine_range * (abs(right_y_signal) * (engine_two_f * engine_e))
+        engine_fiv_memory += engine_range * (abs(right_y_signal) * (engine_fiv_f * engine_e))
+
+        engine_thr_memory -= engine_range * (abs(right_y_signal) * (engine_thr_b * engine_e))
+        engine_fou_memory -= engine_range * (abs(right_y_signal) * (engine_fou_b * engine_e))
+
+    elif right_y_signal > 0:  # + to_the_front engine_b inC engine_f deC
+        engine_one_memory -= engine_range * (abs(right_y_signal) * (engine_one_f * engine_e))
+        engine_two_memory -= engine_range * (abs(right_y_signal) * (engine_two_f * engine_e))
+        engine_fiv_memory -= engine_range * (abs(right_y_signal) * (engine_fiv_f * engine_e))
+
+        engine_thr_memory += engine_range * (abs(right_y_signal) * (engine_thr_b * engine_e))
+        engine_fou_memory += engine_range * (abs(right_y_signal) * (engine_fou_b * engine_e))
+    else:
+        pass
+
+    engine_one_pwm.set_duty_cycle(engine_one_memory)
+    engine_two_pwm.set_duty_cycle(engine_two_memory)
+    engine_thr_pwm.set_duty_cycle(engine_thr_memory)
+    engine_fou_pwm.set_duty_cycle(engine_fou_memory)
+    engine_fiv_pwm.set_duty_cycle(engine_fiv_memory)
+    engine_six_pwm.set_duty_cycle(engine_six_memory)
 
     print("left_x: %f  " % left_x_signal,
           "left_y: %f  " % left_y_signal,
--- CPC_TF.py
+++ CPC_TF.py
@@ -1,7 +1,86 @@
-engine_min = 1.100
-engine_max = 1.940
-engine_cp = 0.25
-engine_range = engine_max - engine_min
-engine_cs = engine_range * (1 - engine_cp) + engine_min
-engine_cp_range = engine_range * engine_cp
-print(engine_cs, engine_cp_range)
+from CPC_CO import *
+rotor_radius = 500
+engine_force = 10000
+
+basic_lever = engine_force * rotor_radius
+
+# engine_one_x = 90
+engine_one_y = 0
+
+engine_two_x = 18
+engine_two_y = 72
+
+engine_thr_x = 54
+engine_thr_y = 36
+
+engine_fou_x = 54
+engine_fou_y = 36
+
+engine_fiv_x = 18
+engine_fiv_y = 72
+
+# engine_one_length_to_x = CO(engine_one_x) * rotor_radius
+engine_one_length_to_y = CO(engine_one_y) * rotor_radius
+
+engine_two_length_to_x = CO(engine_two_x) * rotor_radius
+engine_two_length_to_y = CO(engine_two_y) * rotor_radius
+
+engine_thr_length_to_x = CO(engine_thr_x) * rotor_radius
+engine_thr_length_to_y = CO(engine_thr_y) * rotor_radius
+
+engine_fou_length_to_x = CO(engine_fou_x) * rotor_radius
+engine_fou_length_to_y = CO(engine_fou_y) * rotor_radius
+
+engine_fiv_length_to_x = CO(engine_fiv_x) * rotor_radius
+engine_fiv_length_to_y = CO(engine_fiv_y) * rotor_radius
+
+# engine_one_lever_x = basic_lever / (engine_one_length_to_x * engine_force)
+engine_one_lever_y = basic_lever / (engine_one_length_to_y * engine_force)
+
+engine_two_lever_x = basic_lever / (engine_two_length_to_x * engine_force)
+engine_two_lever_y = basic_lever / (engine_two_length_to_y * engine_force)
+
+engine_thr_lever_x = basic_lever / (engine_thr_length_to_x * engine_force)
+engine_thr_lever_y = basic_lever / (engine_thr_length_to_y * engine_force)
+
+engine_fou_lever_x = basic_lever / (engine_fou_length_to_x * engine_force)
+engine_fou_lever_y = basic_lever / (engine_fou_length_to_y * engine_force)
+
+engine_fiv_lever_x = basic_lever / (engine_fiv_length_to_x * engine_force)
+engine_fiv_lever_y = basic_lever / (engine_fiv_length_to_y * engine_force)
+
+
+# f = engine_125
+
+engine_one_f = engine_one_lever_y / 3
+engine_two_f = engine_two_lever_y / 3
+engine_fiv_f = engine_fiv_lever_y / 3
+
+# r = engine_23
+
+engine_two_r = engine_two_lever_x / 2
+engine_thr_r = engine_thr_lever_x / 2
+
+# b = engine_34
+
+engine_thr_b = engine_thr_lever_y / 2
+engine_fou_b = engine_fou_lever_y / 2
+
+# l = engine 45
+
+engine_fou_l = engine_fou_lever_x / 2
+engine_fiv_l = engine_fiv_lever_x / 2
+
+
+print(0.25 * (engine_one_f * 0.4), "f")
+print(0.25 * (engine_two_f * 0.45), "f")
+print(0.25 * (engine_fiv_f * 0.4), "f \n")
+
+print(0.25 * (engine_two_r * 0.4), "r")
+print(0.25 * (engine_thr_r * 0.4), "r \n")
+
+print(0.25 * (engine_thr_b * 0.5), "b")
+print(0.25 * (engine_fou_b * 0.4), "b \n")
+
+print(0.25 * (engine_fou_l * 0.4), "l")
+print(0.25 * (engine_fiv_l * 0.4), "l \n")
