ce1a4974174fe1794fb77d7ea04d50278dbbf2ab
Sirius1897 <Sirius97black@gmail.com>
2018-12-30 00:28:53 +0100

    asd

--- AccelGyroMag.py
+++ AccelGyroMag.py
@@ -75,17 +75,17 @@ while True:
     imu.read_temp()
     imu.read_mag()
 
-    print("Accelerometer: %-12s" % round(imu.accelerometer_data[0], 8), "%-12s" % round(imu.accelerometer_data[1], 8), "%-16s" % round(imu.accelerometer_data[2], 8),
-          "Gyroscope: %-12s" % round(imu.gyroscope_data[0], 8), "%-12s" % round(imu.gyroscope_data[1], 8), "%-16s" % round(imu.gyroscope_data[2], 8),
-          "Temperature: %-16s" % round(imu.temperature, 8),
-          "Magnetometer: %-12s" % round(imu.magnetometer_data[0], 8), "%-12s" % round(imu.magnetometer_data[1], 8), "%-16s" % round(imu.magnetometer_data[2], 8))
+    # print("Accelerometer: %-12s" % round(imu.accelerometer_data[0], 8), "%-12s" % round(imu.accelerometer_data[1], 8), "%-16s" % round(imu.accelerometer_data[2], 8),
+    #       "Gyroscope: %-12s" % round(imu.gyroscope_data[0], 8), "%-12s" % round(imu.gyroscope_data[1], 8), "%-16s" % round(imu.gyroscope_data[2], 8),
+    #       "Temperature: %-16s" % round(imu.temperature, 8),
+    #       "Magnetometer: %-12s" % round(imu.magnetometer_data[0], 8), "%-12s" % round(imu.magnetometer_data[1], 8), "%-16s" % round(imu.magnetometer_data[2], 8))
 
-    time.sleep(0.01)
+    # time.sleep(0.01)
 
-    # m9a, m9g, m9m = imu.getMotion9()
-    #
-    # print("Acc:", "{:+7.3f}".format(m9a[0]), "{:+7.3f}".format(m9a[1]), "{:+7.3f}".format(m9a[2]),)
-    # print(" Gyr:", "{:+8.3f}".format(m9g[0]), "{:+8.3f}".format(m9g[1]), "{:+8.3f}".format(m9g[2]),)
-    # print(" Mag:", "{:+7.3f}".format(m9m[0]), "{:+7.3f}".format(m9m[1]), "{:+7.3f}".format(m9m[2]))
-    #
-    # time.sleep(0.5)
+    m9a, m9g, m9m = imu.getMotion9()
+
+    print("Acc:", "{:+7.3f}".format(m9a[0]), "{:+7.3f}".format(m9a[1]), "{:+7.3f}".format(m9a[2]),)
+    print(" Gyr:", "{:+8.3f}".format(m9g[0]), "{:+8.3f}".format(m9g[1]), "{:+8.3f}".format(m9g[2]),)
+    print(" Mag:", "{:+7.3f}".format(m9m[0]), "{:+7.3f}".format(m9m[1]), "{:+7.3f}".format(m9m[2]))
+
+    time.sleep(0.5)
