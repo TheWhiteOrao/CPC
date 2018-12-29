a115828f8c52e0b729f6a16c22028d0d92ec54b3
Sirius1897 <Sirius97black@gmail.com>
2018-12-29 17:11:58 +0100

    cx

--- CPC_AHRS.py
+++ CPC_AHRS.py
@@ -82,6 +82,8 @@ def imuLoop():
 
     dt = (currenttime - previoustime) / 1000000
 
+    print(dt)
+
     # -------- Read raw measurements from the MPU and update AHRS - -------------
 
     # Accel + gyro.
@@ -132,11 +134,11 @@ def imuLoop():
     if dtsumm > 0.05:
 
         # Console output
-        print("ROLL: %-26s" % roll,
-              "PITCH: %-26s" % pitch,
-              "YAW: %-26s" % (yaw * -1),
-              "PERIOD %-26s" % dt,
-              "RATE %-26s \n" % int(1 / dt))
+        # print("ROLL: %-26s" % roll,
+        #       "PITCH: %-26s" % pitch,
+        #       "YAW: %-26s" % (yaw * -1),
+        #       "PERIOD %-26s" % dt,
+        #       "RATE %-26s \n" % int(1 / dt))
 
         # Network output
         # sprintf(sendline, "%10f %10f %10f %10f %dHz\n", getW(), getX(), getY(), getZ(), int(1 / dt));
