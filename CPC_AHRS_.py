c4c4bd104b4bbb507e5137eb1a45b21c0d556cf7
Sirius1897 <Sirius97black@gmail.com>
2018-12-29 21:07:20 +0100

    yxc

--- CPC_AHRS_.py
+++ CPC_AHRS_.py
@@ -197,7 +197,11 @@ def getEuler():
     # roll = atan2(2.0 * (q0 * q1 + q2 * q3), q0 * q0 - q1 * q1 - q2 * q2 + q3 * q3) * 180 / pi
     # pitch = -asin(2.0 * (q1 * q3 - q0 * q2)) * 180 / pi
     # yaw = 0 + atan2(2.0 * (q0 * q3 + q1 * q2), q0 * q0 + q1 * q1 - q2 * q2 - q3 * q3) * 180 / pi
+    print(q0, q2, q3, q3)
     return roll, pitch, yaw
 
 
 # endif # AHRS_hpp
+# 0.054440907603739
+# 0.054573651805083
+# 0.057579213696842
