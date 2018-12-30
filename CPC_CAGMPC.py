e53dcef5bdac6a5c3819cff71567296396821aff
Sirius1897 <Sirius97black@gmail.com>
2018-12-30 18:06:34 +0100

    yxc<

--- CPC_CAGMPC.py
+++ CPC_CAGMPC.py
@@ -12,10 +12,11 @@ k = 0
 lsm9ds1.initialize()
 mpu9250.initialize()
 ms5611.initialize()
-for i in range(10000):
-    ms5611.refreshPressure()
-    ms5611.readPressure()
-    k += ms5611.PRES / 10000
+for i in range(1010):
+    if i > 9:
+        ms5611.refreshPressure()
+        ms5611.readPressure()
+        k += ms5611.PRES / 1000
 
 
 print(k)
