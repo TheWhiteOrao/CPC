db209d52a5ad38481fd26337477c769d8e3b5d6b
Sirius1897 <Sirius97black@gmail.com>
2018-12-30 20:11:37 +0100

    yc<x

--- CPC_CAGMPC.py
+++ CPC_CAGMPC.py
@@ -37,4 +37,4 @@ while True:
 
         lol += ((k - ms5611.PRES) * 9.3 + 1) / 20
 
-    print("Temperature(C): %.6f" % (ms5611.TEMP), "Höhe in meter: %.2f" % round(lol, 2))
+    print("Temperature(C): %.6f" % (ms5611.TEMP), "Höhe in meter: %.1f" % (lol)
