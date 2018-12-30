f71b4d7c6790ba5fbebc6531de4ac06744fc2649
Sirius1897 <Sirius97black@gmail.com>
2018-12-30 16:57:26 +0100

    fhh

--- CPC_CAGMPC.py
+++ CPC_CAGMPC.py
@@ -6,3 +6,12 @@ from CPC_NAVIO2.ms5611 import *
 lsm9ds1 = LSM9DS1()
 mpu9250 = MPU9250()
 ms5611 = MS5611()
+
+lsm9ds1.initialize()
+mpu9250.initialize()
+ms5611.initialize()
+
+while True:
+    ms5611.update()
+    h = ms5611.calculatePressureAndTemperature()
+    print(h.TEMP)
