1146468d257753b9ba8ac739e4dd1b2743c1d391
Sirius1897 <Sirius97black@gmail.com>
2018-12-31 00:40:28 +0100

    cyx<

--- CPC_CAGMPC.py
+++ CPC_CAGMPC.py
@@ -31,11 +31,18 @@ while True:
           "lsm_az: %-26s" % lsm_az,
           "lsm_gx: %-26s" % lsm_gx,
           "lsm_gy: %-26s" % lsm_gy,
-          "lsm_gz: %-26s" % lsm_gz, "\n",
-          "mpu_ax: %-26s" % mpu_ax,
+          "lsm_gz: %-26s" % lsm_gz)
+
+    print("mpu_ax: %-26s" % mpu_ax,
           "mpu_ay: %-26s" % mpu_ay,
           "mpu_az: %-26s" % mpu_az,
           "mpu_gx: %-26s" % mpu_gx,
           "mpu_gy: %-26s" % mpu_gy,
-          "mpu_gz: %-26s" % mpu_gz,
-          )
+          "mpu_gz: %-26s" % mpu_gz)
+
+    print("d_ax: %-26s" % ((lsm_ax + mpu_ax) / 2),
+          "d_ay: %-26s" % ((lsm_ay + mpu_ay) / 2),
+          "d_az: %-26s" % ((lsm_az + mpu_az) / 2),
+          "d_gx: %-26s" % ((lsm_gx + mpu_gx) / 2),
+          "d_gy: %-26s" % ((lsm_gy + mpu_gy) / 2),
+          "d_gz: %-26s" % ((lsm_gz + mpu_gz) / 2))
