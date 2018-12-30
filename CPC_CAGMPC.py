d0e7b5e45fb6489d05dfd62fe3907df89965d7f6
Sirius1897 <Sirius97black@gmail.com>
2018-12-30 21:24:47 +0100

    xyv

---
+++ CPC_CAGMPC.py
@@ -0,0 +1,15 @@
+from CPC_NAVIO2.lsm9ds1 import *
+from CPC_NAVIO2.mpu9250 import *
+
+lsm9ds1 = LSM9DS1()
+mpu9250 = MPU9250()
+
+lsm9ds1.initialize()
+mpu9250.initialize()
+
+while True:
+
+lsm = lsm9ds1.getMotion9
+mpu = mpu9250.getMotion9
+
+print(lsm, mpu)
