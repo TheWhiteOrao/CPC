44675033e4a27e4e13676b6a5031a3df5f8bc4d7
Sirius1897 <Sirius97black@gmail.com>
2019-01-01 19:44:19 +0100

    yc<x

--- CPC_ROL_PIT_YAW/CPC_SENSOR_READ.py
+++ CPC_ROL_PIT_YAW/CPC_SENSOR_READ.py
@@ -14,12 +14,26 @@ while True:
     acc_mpu, gyr_mpu, mag_mpu, tem_mpu = mpu9250_sensor_read(mpu9250)
     acc_lsm, gyr_lsm, mag_lsm, tem_lsm = lsm9ds1_sensor_read(lsm9ds1)
 
-    print("acc_mpu: %-26s" % acc_mpu,
-          "gyr_mpu: %-26s" % gyr_mpu,
-          "mag_mpu: %-26s" % mag_mpu,
-          "tem_mpu: %-26s" % tem_mpu)
+    print("acc_mpu: %-26s" % acc_mpu[0],
+          "%-26s" % acc_mpu[1],
+          "%-26s" % acc_mpu[2],
+          "gyr_mpu: %-26s" % gyr_mpu[0],
+          "%-26s" % gyr_mpu[1],
+          "%-26s" % gyr_mpu[2],
+          "mag_mpu: %-26s" % mag_mpu[0],
+          "%-26s" % mag_mpu[1],
+          "%-26s" % mag_mpu[2],
+          "tem_mpu: %-26s" % tem_mpu
+          )
 
-    print("acc_lsm: %-26s" % acc_lsm,
-          "gyr_lsm: %-26s" % gyr_lsm,
-          "mag_lsm: %-26s" % mag_lsm,
-          "tem_lsm: %-26s" % tem_lsm)
+    print("acc_lsm: %-26s" % acc_lsm[0],
+          "%-26s" % acc_lsm[1],
+          "%-26s" % acc_lsm[2],
+          "gyr_lsm: %-26s" % gyr_lsm[0],
+          "%-26s" % gyr_lsm[1],
+          "%-26s" % gyr_lsm[2],
+          "mag_lsm: %-26s" % mag_lsm[0],
+          "%-26s" % mag_lsm[1],
+          "%-26s" % mag_lsm[2],
+          "tem_lsm: %-26s" % tem_lsm
+          )
