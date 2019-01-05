5b62ea99bdc845b689fdd3042d8121de9374c176
Sirius1897 <Sirius97black@gmail.com>
2019-01-05 01:29:14 +0100

    l

---
+++ CPC_ROL_PIT_YAW/Control_Penta_Coptter/Sensor_Read.py
@@ -0,0 +1,22 @@
+
+
+# Sensor read, reads the sensors parameters and returns them as a tuple: [0] = accelerometer data, [1] = gyroscope data, [3] = temperature of the sensor.
+# Sensors that can be used are mup9250 and lsm9ds1.
+
+def sensor_read(sensor):
+
+    sensor.read_acc()
+    sensor.read_gyro()
+    sensor.read_temp()
+
+    return sensor.accelerometer_data, sensor.gyroscope_data, sensor.temperature
+
+
+if __name__ == '__main__':
+    from N2.mup9250 import *
+
+    mpu9250 = MPU9250()
+
+    mpu9250.initialize()
+
+    print(sensor_read(mpu9250))
