43129a4252fc7ddc413b7cd78119e959ad6be674
Sirius1897 <Sirius97black@gmail.com>
2019-01-05 01:14:05 +0100

    sfda

--- CPC_ROL_PIT_YAW/Control_Penta_Coptter/Micro_Sleep.py
+++ CPC_ROL_PIT_YAW/Control_Penta_Coptter/Micro_Sleep.py
@@ -1,11 +1,11 @@
 from time import sleep
 
+
 # Microsecond sleep time
-# x are in seconds and while convert to microsecond
+# seconds  while convert to microsecond
 
-
-def micro_sleep(x):
-    return sleep(x * 0.000001)
+def micro_sleep(seconds):
+    return sleep(seconds * 0.000001)
 
 
 if __name__ == '__main__':
