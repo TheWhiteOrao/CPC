416bed756a0c9ec7b8216e91237a9eef3258209f
Sirius1897 <Sirius97black@gmail.com>
2019-01-07 23:06:26 +0100

    fd

--- CPC_ROL_PIT_YAW/Control_Penta_Copter/Pulsdauermodulation.py
+++ CPC_ROL_PIT_YAW/Control_Penta_Copter/Pulsdauermodulation.py
@@ -1,17 +1,15 @@
 from N2.pwm import *
 
 
-def pulsdauermodulation(number_of_engien):
+def pulsdauermodulation(number_of_engien, refresh_frequency=500, pos_range_max=1100, pos_range_may=1940):
 
     engine = dict(map(lambda x: (x, 0), range(number_of_engien)))
 
     for i in range(number_of_engien):
         engine[i] = PWM(i)
-
-    print(engine)
-
-    for i in range(number_of_engien):
         engine[i].initialize()
+        engine[i].set_period(refresh_frequency)
+        engine[i].enable()
 
     print(engine)
 
