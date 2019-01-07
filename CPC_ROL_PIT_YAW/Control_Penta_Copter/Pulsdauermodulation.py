691727baa5280819e3fc2778cf664a879d6320c0
Sirius1897 <Sirius97black@gmail.com>
2019-01-07 22:26:57 +0100

    xvy

---
+++ CPC_ROL_PIT_YAW/Control_Penta_Copter/Pulsdauermodulation.py
@@ -0,0 +1,18 @@
+from N2.pwm import *
+
+
+def pulsdauermodulation(number_of_engien):
+
+    engine = dict(map(lambda x: (x, 0), range(number_of_engien)))
+
+    for i in range(number_of_engien):
+        engine[i] = PWM(i)
+
+    for i in range(number_of_engien):
+        engine[i].initialize()
+
+    print(engine)
+
+
+if __name__ == '__main__':
+    pulsdauermodulation(5)
