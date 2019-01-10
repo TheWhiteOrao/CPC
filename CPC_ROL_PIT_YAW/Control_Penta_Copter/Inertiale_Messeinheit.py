3556164ec55091c96bf0e7a7ac34508ca746fba3
Sirius1897 <Sirius97black@gmail.com>
2019-01-11 00:12:24 +0100

    vcy

--- CPC_ROL_PIT_YAW/Control_Penta_Copter/Inertiale_Messeinheit.py
+++ CPC_ROL_PIT_YAW/Control_Penta_Copter/Inertiale_Messeinheit.py
@@ -3,8 +3,14 @@
 # output: roll and pitch not converted in a dictionary. like this {roll:deg, pitch:deg}
 
 
-def inertiale_messeinheit(sensor_data)
+def inertiale_messeinheit(sensor_data):
+    pass
 
 
 if __name__ == '__main__':
+    from Sensor_Initialize import sensor_initialize
     from Sensor_Read import sensor_read
+
+    sensor = sensor_initialize("mpu9250")
+    sensor_data = sensor_read(sensor)
+    print(sensor_data)
