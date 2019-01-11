5344d0dea5fc809041b08347e8c97ee342b76b35
Sirius1897 <Sirius97black@gmail.com>
2019-01-11 20:22:04 +0100

    nvb

---
+++ CPC_ROL_PIT_YAW/Control_Penta_Copter/Inertial_Measurement_Unit.py
@@ -0,0 +1,95 @@
+
+# imput: sensor read MPU9250 accelerometer_data, gyroscope_data
+# output: roll and pitch not converted in a dictionary. like this {roll:deg, pitch:deg}
+
+from datetime import datetime
+from math import atan2, sqrt, asin, cos
+
+RAD_TO_DEG = 57.29578
+M_PI = 3.14159265358979323846
+G_GAIN = 0.070  # [deg/s/LSB]  If you change the dps for gyro, you need to update this value accordingly
+AA = 0.40      # Complementary filter constant
+
+gyroXangle = 0.0
+gyroYangle = 0.0
+gyroZangle = 0.0
+CFangleX = 0.0
+CFangleY = 0.0
+
+
+def inertial_measurement_unit(sensor_data):
+
+    global RAD_TO_DEG
+    global M_PI
+    global G_GAIN
+    global AA
+    global gyroXangle
+    global gyroYangle
+    global gyroZangle
+    global CFangleX
+    global CFangleY
+
+    # Read the accelerometer and gyroscope values
+    ACCx = sensor_data["acce"]["ax"]
+    ACCy = sensor_data["acce"]["ay"]
+    ACCz = sensor_data["acce"]["az"]
+    GYRx = sensor_data["gyro"]["gx"]
+    GYRy = sensor_data["gyro"]["gy"]
+    GYRz = sensor_data["gyro"]["gz"]
+
+    # Calculate loop Period(LP). How long between Gyro Reads
+    b = datetime.datetime.now() - a
+    a = datetime.datetime.now()
+    LP = b.microseconds / (1000000 * 1.0)
+
+    # Convert Gyro raw to degrees per second
+    rate_gyr_x = GYRx * G_GAIN
+    rate_gyr_y = GYRy * G_GAIN
+    rate_gyr_z = GYRz * G_GAIN
+
+    # Calculate the angles from the gyro.
+    gyroXangle += rate_gyr_x * LP
+    gyroYangle += rate_gyr_y * LP
+    gyroZangle += rate_gyr_z * LP
+
+    # Convert Accelerometer values to degrees
+    AccXangle = (math.atan2(ACCy, ACCz) * RAD_TO_DEG)
+    AccYangle = (math.atan2(ACCz, ACCx) + M_PI) * RAD_TO_DEG
+
+    # convert the values to -180 and +180
+    if AccYangle > 90:
+        AccYangle -= 270.0
+    else:
+        AccYangle += 90.0
+
+    # Complementary filter used to combine the accelerometer and gyro values.
+    CFangleX = AA * (CFangleX + rate_gyr_x * LP) + (1 - AA) * AccXangle
+    CFangleY = AA * (CFangleY + rate_gyr_y * LP) + (1 - AA) * AccYangle
+
+    # Normalize accelerometer raw values.
+    accXnorm = ACCx / math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
+    accYnorm = ACCy / math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
+
+    # Calculate pitch and roll
+    pitch = math.asin(accXnorm)
+    roll = -math.asin(accYnorm / math.cos(pitch))
+
+    return pitch, roll
+    # print("ax: %-26s" % sensor_data["acce"]["ax"],
+    #       "ay: %-26s" % sensor_data["acce"]["ay"],
+    #       "az: %-26s" % sensor_data["acce"]["az"],
+    #       "gx: %-26s" % sensor_data["gyro"]["gx"],
+    #       "gy: %-26s" % sensor_data["gyro"]["gy"],
+    #       "gz: %-26s" % sensor_data["gyro"]["gz"],
+    #       "te: %-26s" % sensor_data["temp"])
+
+
+if __name__ == '__main__':
+    from Sensor_Initialize import sensor_initialize
+    from Sensor_Read import sensor_read
+
+    sensor = sensor_initialize("mpu9250")
+
+    while True:
+        IMU = inertial_measurement_unit(sensor_read(sensor))
+        print(IMU)
--- CPC_ROL_PIT_YAW/Control_Penta_Copter/Test.py
+++ CPC_ROL_PIT_YAW/Control_Penta_Copter/Test.py
@@ -42,12 +42,108 @@
 #     print(1000000000 / (process_time_ns() - h))
 #     h = process_time_ns()
 
+#
+# h = {1: 0}
+#
+# if h == dict():
+#     print("emty")
+# elif type(h) == dict:
+#     print("full")
+# else:
+#     print("rong type")
+#!/usr/bin/python
+#	This is the base code needed to get usable angles from a BerryIMU
+#	using a Complementary filter. The readings can be improved by
+#	adding more filters, E.g Kalman, Low pass, median filter, etc..
+#   See berryIMU.py for more advanced code.
+#
+#	For this code to work correctly, BerryIMU must be facing the
+#	correct way up. This is when the Skull Logo on the PCB is facing down.
+#
+#   Both the BerryIMUv1 and BerryIMUv2 are supported
+#
+#	http://ozzmaker.com/
 
-h = {1: 0}
 
-if h == dict():
-    print("emty")
-elif type(h) == dict:
-    print("full")
-else:
-    print("rong type")
+import time
+import math
+import IMU
+import datetime
+import os
+
+
+RAD_TO_DEG = 57.29578
+M_PI = 3.14159265358979323846
+G_GAIN = 0.070  # [deg/s/LSB]  If you change the dps for gyro, you need to update this value accordingly
+AA = 0.40      # Complementary filter constant
+
+################# Compass Calibration values ############
+# Use calibrateBerryIMU.py to get calibration values
+# Calibrating the compass isnt mandatory, however a calibrated
+# compass will result in a more accurate heading values.
+
+
+gyroXangle = 0.0
+gyroYangle = 0.0
+gyroZangle = 0.0
+CFangleX = 0.0
+CFangleY = 0.0
+
+
+IMU.detectIMU()  # Detect if BerryIMUv1 or BerryIMUv2 is connected.
+IMU.initIMU()  # Initialise the accelerometer, gyroscope and compass
+
+
+a = datetime.datetime.now()
+
+
+while True:
+
+    # Read the accelerometer,gyroscope values
+    ACCx = IMU.readACCx()
+    ACCy = IMU.readACCy()
+    ACCz = IMU.readACCz()
+    GYRx = IMU.readGYRx()
+    GYRy = IMU.readGYRy()
+    GYRz = IMU.readGYRz()
+
+    # Calculate loop Period(LP). How long between Gyro Reads
+    b = datetime.datetime.now() - a
+    a = datetime.datetime.now()
+    LP = b.microseconds / (1000000 * 1.0)
+    print "Loop Time | %5.2f|" % (LP),
+
+    # Convert Gyro raw to degrees per second
+    rate_gyr_x = GYRx * G_GAIN
+    rate_gyr_y = GYRy * G_GAIN
+    rate_gyr_z = GYRz * G_GAIN
+
+    # Calculate the angles from the gyro.
+    gyroXangle += rate_gyr_x * LP
+    gyroYangle += rate_gyr_y * LP
+    gyroZangle += rate_gyr_z * LP
+
+    # Convert Accelerometer values to degrees
+    AccXangle = (math.atan2(ACCy, ACCz) * RAD_TO_DEG)
+    AccYangle = (math.atan2(ACCz, ACCx) + M_PI) * RAD_TO_DEG
+
+    # convert the values to -180 and +180
+    if AccYangle > 90:
+        AccYangle -= 270.0
+    else:
+        AccYangle += 90.0
+
+    # Complementary filter used to combine the accelerometer and gyro values.
+    CFangleX = AA * (CFangleX + rate_gyr_x * LP) + (1 - AA) * AccXangle
+    CFangleY = AA * (CFangleY + rate_gyr_y * LP) + (1 - AA) * AccYangle
+
+    # Normalize accelerometer raw values.
+    accXnorm = ACCx / math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
+    accYnorm = ACCy / math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
+
+    # Calculate pitch and roll
+    pitch = math.asin(accXnorm)
+    roll = -math.asin(accYnorm / math.cos(pitch))
+
+    # slow program down a bit, makes the output more readable
+    time.sleep(0.03)
