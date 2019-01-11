b8c3781c47bf168e0f065eafd80eed96c67d89a6
Sirius1897 <Sirius97black@gmail.com>
2019-01-11 21:56:25 +0100

    vx

--- CPC_ROL_PIT_YAW/Control_Penta_Copter/Inertial_Measurement_Unit.py
+++ CPC_ROL_PIT_YAW/Control_Penta_Copter/Inertial_Measurement_Unit.py
@@ -9,7 +9,7 @@ from cmath import asin, cos
 RAD_TO_DEG = 57.29578
 M_PI = 3.14159265358979323846
 G_GAIN = 0.07  # 0.070  # [deg/s/LSB]  If you change the dps for gyro, you need to update this value accordingly
-AA = 0.8  # 0.40      # Complementary filter constant
+AA = 0.0000000004  # 0.40      # Complementary filter constant
 
 gyroXangle = 0.0
 gyroYangle = 0.0
