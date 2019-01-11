fe07f547704a6ccdce74947e74007f96a1d2a82f
Sirius1897 <Sirius97black@gmail.com>
2019-01-11 23:14:14 +0100

    fdh

--- CPC_ROL_PIT_YAW/Control_Penta_Copter/Gyrometer_Calibration.py
+++ CPC_ROL_PIT_YAW/Control_Penta_Copter/Gyrometer_Calibration.py
@@ -1,24 +1,35 @@
 from Sensor_Read import sensor_read
 from Micro_Sleep import micro_sleep
 
+PI = 3.14159265358
 
-def gyroscope_calibration(sensor_type, n=100):
-    gyr_offset = [0, 0, 0]
 
-    for i in range(n):
-        acc_sen, gyr_sen, tem_sen = sensor_read(sensor_type)
+def gyroscope_calibration(sensor_type, interval=100):
 
-        gyr_x = gyr_sen[0]
-        gyr_y = gyr_sen[1]
-        gyr_z = gyr_sen[2]
+    gyroscope_offset = [0, 0, 0]
 
-        gyr_x *= 180 / PI
-        gyr_y *= 180 / PI
-        gyr_z *= 180 / PI
+    for i in range(interval):
 
-        gyr_offset[0] += (-gyr_x * 0.0175) / n
-        gyr_offset[1] += (-gyr_y * 0.0175) / n
-        gyr_offset[2] += (-gyr_z * 0.0175) / n
-        usleep(10000)
+        sensor_data = sensor_read(sensor_type)
 
-    return gyr_offset
+        gyroscopeX = sensor_data["gyro"]["gx"]
+        gyroscopeY = sensor_data["gyro"]["gy"]
+        gyroscopeZ = sensor_data["gyro"]["gz"]
+
+        gyroscopeX *= 180 / PI
+        gyroscopeY *= 180 / PI
+        gyroscopeZ *= 180 / PI
+
+        gyroscope_offset[0] += (-gyroscopeX * 0.0175) / interval
+        gyroscope_offset[1] += (-gyroscopeY * 0.0175) / interval
+        gyroscope_offset[2] += (-gyroscopeZ * 0.0175) / interval
+
+        micro_sleep(10000)
+
+    return gyroscope_offset
+
+
+if __name__ == '__main__':
+    from Sensor_Initialize import sensor_initialize
+
+    print(gyroscope_calibration(sensor_initialize("mpu9250")))
