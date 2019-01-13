0bf3e6c2feae9f33705fd5a0f594f9728699c63c
Sirius1897 <Sirius97black@gmail.com>
2019-01-13 14:36:24 +0100

    c<yx

---
+++ CPC_ROL_PIT_YAW/Control_Penta_Copter/Socket_Blender.py
@@ -0,0 +1,19 @@
+from socket import *
+
+socket_blender = socket(AF_INET, SOCK_STREAM)
+
+
+# print(gethostbyname(gethostname()))
+IP_server = ("192.168.43.243", 46598)
+socket_blender.bind(IP_server)
+socket_blender.listen(1)
+
+while True:
+    try:
+        (socket_control_penta_copter, IP_client) = socket_blender.accept()
+        msg = eval(socket_control_penta_copter.recv(1024))
+
+    except:
+        pass
+
+    print(msg)
---
+++ CPC_ROL_PIT_YAW/Control_Penta_Copter/Socket_Control_Penta_Copter.py
@@ -0,0 +1,10 @@
+from socket import *
+
+socket_control_penta_copter = socket(AF_INET, SOCK_STREAM)
+
+IP_server = ("192.168.43.243", 46598)
+socket_control_penta_copter.connect(IP_server)
+
+H = str({"H": 1})
+
+socket_control_penta_copter.send(H)
