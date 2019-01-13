8e6aed11777608b0aff378783210e4d34a854994
Sirius1897 <Sirius97black@gmail.com>
2019-01-13 15:18:33 +0100

    sadf

--- CPC_ROL_PIT_YAW/Control_Penta_Copter/Socket_Control_Penta_Copter.py
+++ CPC_ROL_PIT_YAW/Control_Penta_Copter/Socket_Control_Penta_Copter.py
@@ -5,8 +5,6 @@ socket_control_penta_copter = socket(AF_INET, SOCK_STREAM)
 IP_server = ("192.168.43.243", 5555)
 socket_control_penta_copter.connect(IP_server)
 
-H = str({"H": 1})
 
 while True:
     print("run")
-    socket_control_penta_copter.send(bytes(H, "utf8"))
