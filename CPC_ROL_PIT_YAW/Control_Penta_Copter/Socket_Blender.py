61767c619161ff968b5f6e096fed0e932ca9fce8
Sirius1897 <Sirius97black@gmail.com>
2019-01-13 15:42:51 +0100

    yxc<

--- CPC_ROL_PIT_YAW/Control_Penta_Copter/Socket_Blender.py
+++ CPC_ROL_PIT_YAW/Control_Penta_Copter/Socket_Blender.py
@@ -6,7 +6,7 @@ socket_control_penta_copter = socket(AF_INET, SOCK_STREAM)
 IP_server = ("192.168.43.34", 12356)
 socket_control_penta_copter.connect(IP_server)
 
-    print("run")
+print("run")
 while True:
 
     try:
