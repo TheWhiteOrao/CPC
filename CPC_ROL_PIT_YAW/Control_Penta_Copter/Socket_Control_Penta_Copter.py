88b188c6b00694cdcd64c62325b3621e1f6437cd
Sirius1897 <Sirius97black@gmail.com>
2019-01-13 15:32:41 +0100

    cyx<

--- CPC_ROL_PIT_YAW/Control_Penta_Copter/Socket_Control_Penta_Copter.py
+++ CPC_ROL_PIT_YAW/Control_Penta_Copter/Socket_Control_Penta_Copter.py
@@ -2,7 +2,7 @@ from socket import *
 
 socket_control_penta_copter = socket(AF_INET, SOCK_STREAM)
 
-IP_server = ("192.168.43.34", 5555)
+IP_server = ("192.168.43.34", 12345)
 socket_control_penta_copter.connect(IP_server)
 
 
