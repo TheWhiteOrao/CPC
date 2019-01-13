fd90272d8086a576f7021ddd906bbd1fba7b7312
Sirius1897 <Sirius97black@gmail.com>
2019-01-13 04:56:45 +0100

    xcy<

--- CPC_ROL_PIT_YAW/Control_Penta_Copter/Inertial_Measurement_Unit.py
+++ CPC_ROL_PIT_YAW/Control_Penta_Copter/Inertial_Measurement_Unit.py
@@ -154,7 +154,7 @@ if __name__ == '__main__':
     from Gyrometer_Calibration import gyroscope_calibration
     from Delta_Time import calculate_delta_time
     from Euler_Angle import euler_angle
-    from math import acos
+    from math import acos, pi
 
     sensor = sensor_initialize("lsm9ds1")
 
@@ -169,10 +169,10 @@ if __name__ == '__main__':
         delta_time, Hz, current_delta_time = calculate_delta_time(current_delta_time, Hz)
 
         Quaternion = inertial_measurement_unit(sensor_read(sensor), gyroscope_offset, delta_time, Quaternion)
-        print("QuaternionR: %-26s" % (acos(Quaternion["QuaternionR"]) * 2),
-              "QuaternionI: %-26s" % (acos(Quaternion["QuaternionI"]) * 2),
-              "QuaternionJ: %-26s" % (acos(Quaternion["QuaternionJ"]) * 2),
-              "QuaternionK: %-26s" % (acos(Quaternion["QuaternionK"]) * 2))
+        print("QuaternionR: %-26s" % ((acos(Quaternion["QuaternionR"]) * 2) * 180 / pi),
+              "QuaternionI: %-26s" % ((acos(Quaternion["QuaternionI"]) * 2) * 180 / pi),
+              "QuaternionJ: %-26s" % ((acos(Quaternion["QuaternionJ"]) * 2) * 180 / pi),
+              "QuaternionK: %-26s" % ((acos(Quaternion["QuaternionK"]) * 2) * 180 / pi))
 
 #  quat [-0.3832534449239102, -0.00516282644230581, 0.014133308871725835, 0.9235206504228362] RATE 599
 #
