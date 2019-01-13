4c6a4edb3235eef84daed433d8230174ffca882e
Sirius1897 <Sirius97black@gmail.com>
2019-01-13 15:42:48 +0100

    yx<c

--- CPC_ROL_PIT_YAW/Control_Penta_Copter/Socket_Blender.py
+++ CPC_ROL_PIT_YAW/Control_Penta_Copter/Socket_Blender.py
@@ -1,20 +1,15 @@
+
 from socket import *
-#Blender = Server
 
+socket_control_penta_copter = socket(AF_INET, SOCK_STREAM)
 
-# print(gethostbyname(gethostname()))
-host = "192.168.43.34"
-port = 12356
+IP_server = ("192.168.43.34", 12356)
+socket_control_penta_copter.connect(IP_server)
 
-server_socket = socket(AF_INET, SOCK_STREAM)
+    print("run")
+while True:
 
-try:
-    server_socket.bind((host, port))
-except:
-    pass
-
-server_socket.listen(1)
-
-conn, addr = server_socket._accept()
-12356
-print(addr)
+    try:
+        print(socket_control_penta_copter.recv(2048))
+    except:
+        pass
